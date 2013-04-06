0728963efbea97cab5d8f04bd636aa0165a49252
Mattia Basaglia <mattia.basaglia@gmail.com>
2013-04-06 20:58:27 +0200

    Screenshot

--- main.cpp
+++ main.cpp
@@ -5,8 +5,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Color_Dialog w;
-    w.setColor(QColor(128,255,16,128));
+    w.setColor(QColor(172,169,64,128));
     w.show();
+
+    /*QPixmap p(w.size());
+    w.render(&p);
+    p.save("screenshot.png");*/
     
     return a.exec();
 }
