d93632e6cfbb11cf2914a578895a442e017e70e7
Mattia Basaglia <mattia.basaglia@gmail.com>
2013-04-07 10:53:36 +0200

    Readmes

--- main.cpp
+++ main.cpp
@@ -27,11 +27,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Color_Selector s;
-    s.show();
-    /*Color_Dialog w;
+    /*Color_Selector s;
+    s.show();*/
+    Color_Dialog w;
     w.setColor(QColor(172,169,64,128));
-    w.show();*/
+    w.show();
 
     /*QPixmap p(w.size());
     w.render(&p);
